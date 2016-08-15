0316dc3fe85e4f3a597ab9dad207cb9af72fd06e
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-08-15 16:05:05 -0300

    Add amp as mime-type and create isolate css file to be included in-line

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module AmpStudies
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile << 'amp/application.scss'
   end
 end
